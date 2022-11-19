acf64c7f34301cd55cfac37e18c8101d10dcbaff
Aldo Gonzalez <aldo.linux@gmail.com>
2022-11-19 11:08:33 -0500

    Migrations work in local Ganache

--- truffle.js
+++ truffle.js
@@ -1,14 +1,14 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+var mnemonic = "reflect strong mixed space rug swallow insane figure estate lens grit drama";
 
 module.exports = {
   networks: {
     development: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 50);
       },
       network_id: '*',
-      gas: 9999999
+      gas: 4500000
     }
   },
   compilers: {
